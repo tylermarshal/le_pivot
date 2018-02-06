f31f08c941c66e3b4116689fc528ff1a128823f1
TMad <madsen.tyler@gmail.com>
2018-02-06 12:44:45 -0700

    add store-item relationships

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,12 @@
 class Store < ApplicationRecord
+  has_many :items
+
+  validates_uniqueness_of :name, :slug
+
+  before_validation :generate_slug
+
+  def generate_slug
+    self.slug = name.parameterize
+  end
+
 end
