fcac55cfb2cb9ba5aaf0d4833fec313fbf2ef8d6
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-07 21:25:35 -0700

    make user_cant_see_add_to_cart_for_retired_item_in_show_spec pass; 9 failing tests

--- spec/features/user/items/user_cant_see_add_to_cart_for_retired_item_in_show_spec.rb
+++ spec/features/user/items/user_cant_see_add_to_cart_for_retired_item_in_show_spec.rb
@@ -3,12 +3,9 @@ require 'rails_helper'
 RSpec.describe 'As a visitor' do
 	describe 'when visiting an items show page' do
 		it 'a user cannot see add to cart for a retired item' do
-			category = Category.create(title: "Animals")
-			one_url = "http://pandathings.com/wp-content/uploads/2016/10/onesie-6-300x300.png"
-			item_one = category.items.create(title: "Funsie Onesie", description: "number one", price: 8.00,
-			image: one_url, condition: 'retired' )
+			item = create(:item, condition: 'retired')
 			
-			visit item_path(item_one)
+			visit store_item_path(item.store.slug, item)
 			
 			expect(page).not_to have_content("Add to cart")
 		end
