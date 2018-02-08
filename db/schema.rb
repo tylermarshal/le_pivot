f0ce2fccade38cf10d0c9811468b0f43dfbe6a55
Timo Tyrrell <timomitchel23@gmail.com>
2018-02-08 16:31:17 -0700

    one more require admin and current admin removed and roles and user_roles migration

--- db/schema.rb
+++ db/schema.rb
@@ -62,12 +62,27 @@ ActiveRecord::Schema.define(version: 20180208212755) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.integer "status", default: 0
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_user_roles_on_role_id"
+    t.index ["user_id"], name: "index_user_roles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -86,4 +101,6 @@ ActiveRecord::Schema.define(version: 20180208212755) do
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
