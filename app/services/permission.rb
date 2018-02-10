780dcc979d9ef787e146f67682121ca1f809b34f
Timo Tyrrell <timomitchel23@gmail.com>
2018-02-09 18:51:51 -0700

    add authorized permission class

---
+++ app/services/permission.rb
@@ -0,0 +1,66 @@
+class Permission
+  def initialize(user, controller, action)
+    @user = user || User.new
+    @controller = controller
+    @action = action
+  end
+
+  def authorized?
+    if user.platform_admin?
+      return true if controller == "users" && action.in?(["index", "show", "edit", "update", "new", "create"])
+      return true if controller == "store/items" && action == "index"
+      return true if controller == "carts" && action == "index"
+      return true if controller == "admin/dashboard" && action == "index"
+      return true if controller == "/dashboard" && action == "index"
+      return true if controller == "admin/analytics" && action == "index"
+      return true if controller == "admin/stores/items" && action.in?(["index", "new", "create", "edit", "update"])
+      return true if controller == "main" && action == "index"
+      return true if controller == "orders" && action.in?(["index", "show"])
+      return true if controller == "items" && action.in?(["index", "show"])
+      return true if controller == "stores" && action == "index"
+      return true if controller == "sessions" && action.in?(["create", "new", "destroy"])
+      return true if controller == "categories" && action == "show"
+    elsif user.store_admin? 
+      return true if controller == "categories" && action == "show"      
+      return true if controller == "users" && action.in?(["index", "show", "edit", "update", "new", "create"])      
+      return true if controller == "carts" && action == "index"      
+      return true if controller == "store/items" && action =="index"
+      return true if controller == "main" && action =="index"
+      return true if controller == "items" && action.in?(["index", "show"])
+      return true if controller == "stores" && action == "index"
+      return true if controller == "sessions" && action.in?(["create", "new", "destroy"])
+      return true if controller == "admin/dashboard" && action == "index"
+      return true if controller == "/dashboard" && action == "index" 
+      return true if controller == "admin/analytics" && action == "index"
+    elsif user.store_manager?
+      return true if controller == "categories" && action == "show"      
+      return true if controller == "users" && action.in?(["index", "show", "edit", "update", "new", "create"])      
+      return true if controller == "carts" && action == "index"      
+      return true if controller == "store/items" && action == "index"
+      return true if controller == "main" && action == "index"
+      return true if controller == "stores" && action == "index"
+      return true if controller == "sessions" && action.in?(["create", "new", "destroy"])
+      return true if controller == "items" && action.in?(["index", "show"])
+    elsif user.registered_user? 
+      return true if controller == "categories" && action == "show"      
+      return true if controller == "users" && action.in?(["index", "show", "edit", "update", "new", "create"])      
+      return true if controller == "carts" && action == "index"      
+      return true if controller == "main" && action == "index"
+      return true if controller == "stores" && action == "index"
+      return true if controller == "sessions" && action.in?(["create", "new", "destroy"])
+      return true if controller == "items" && action.in?(["index", "show"])
+    else
+      return true if controller == "categories" && action == "show"      
+      return true if controller == "carts" && action == "index"      
+      return true if controller == "main" && action == "index"
+      return true if controller == "stores" && action == "index"
+      return true if controller == "sessions" && action.in?(["create", "new", "destroy"])
+      return true if controller == "items" && action.in?(["index", "show"])
+      return true if controller == "users" && action.in?(["create", "new"])      
+    end
+  end
+
+  private
+
+    attr_reader :user, :controller, :action
+end
