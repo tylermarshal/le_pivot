83ccbd8dfb03414c1aa39683bd1af3dd9c8d998f
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-12 14:42:54 -0700

    finish user_can_see_purchase_history_spec and make sure it passes

--- spec/features/user/orders/user_can_see_purchase_history_spec.rb
+++ spec/features/user/orders/user_can_see_purchase_history_spec.rb
@@ -3,10 +3,16 @@ require 'rails_helper'
 feature "a registered user can see their purchase history" do
   it "as a registered user" do
     user = create(:user)
+    role = Role.create(title: "registered_user")
+    user.roles << role
     order = create(:order, user: user)
-    binding.pry
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
     visit '/orders'
 
-    expect()
+    expect(page).to have_content(order.id)
+    expect(page).to have_content(order.date)
+    expect(page).to have_content(order.status.capitalize)
+    expect(page).to have_content(order.total_price)
   end
 end
