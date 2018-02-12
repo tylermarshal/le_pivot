03df84a255a01d1837f41f9998db455ff8fd7102
katyjane8 <katy.jane8@gmail.com>
2018-02-12 13:57:08 -0700

    both tests passing for guest visiting businesses and adding items to cart

--- spec/features/guest_user/items/visitor_can_add_items_from_mutiliple_stores_to_cart_spec.rb
+++ spec/features/guest_user/items/visitor_can_add_items_from_mutiliple_stores_to_cart_spec.rb
@@ -1,15 +1,28 @@
-RSpec.feature "Adding items to the cart" do
-  it "can visit one store" do
+require 'rails_helper'
+
+describe "As a visitor can visit different stores and add items to the cart" do
+  it "can visit one store first and add items to the cart" do
     store1 = create(:store, name: "Plants")
-    store2 = create(:store, name: "Books")
-    create(:item, title: "Dove", store: store1)
-    create(:item, title: "Light Saber", store: store2)
+    category = create(:category, title: "Plants")
+    create(:item, title: "Dove", store: store1, category: category)
 
-    visit '/stores'
+    visit '/plants'
 
-    within(".stores") do
-      expect(page).to have_content("Plants")
-      expect(page).to have_content("Books")
-    end
+    expect(page).to have_content("Dove")
+    click_on "Add to cart"
+    expect(page).to have_content("You now have 1 Dove")
+  end
+
+  it "can visit a second store and add items to the cart" do
+    store2 = create(:store, name: "Candles")
+    category1 = create(:category, title: "Candles")
+    create(:item, title: "Light Saber", store: store2, category: category1)
+
+    visit '/candles'
+    expect(page).to have_content("Light Saber")
+    click_link "Add to cart"
+    expect(page).to have_content("You now have 1 Light Saber")
+    click_on "Cart"
+    expect(page).to have_content("Light Saber", "Dove")
   end
 end
