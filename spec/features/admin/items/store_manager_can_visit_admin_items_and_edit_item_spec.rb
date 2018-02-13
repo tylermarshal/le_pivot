29d43321406ab0d3e4681a11e55b732d2cc8b7c5
TMad <madsen.tyler@gmail.com>
2018-02-13 11:14:41 -0700

    edit store manager edit feature test

--- spec/features/admin/items/store_manager_can_visit_admin_items_and_edit_item_spec.rb
+++ spec/features/admin/items/store_manager_can_visit_admin_items_and_edit_item_spec.rb
@@ -16,21 +16,6 @@ RSpec.describe 'an admin can visit admin dashboard' do
       admin.stores << store
       admin_2.stores << store_2
     end
-    it 'when clicked that link should be the admin item index with admin functionality' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-
-      visit admin_store_items_path(item.store.slug)
-
-      click_on "Edit"
-      fill_in "item[title]", with: "White Cat Twosie"
-      fill_in "item[description]", with: "two is better"
-      fill_in "item[price]", with: "39.99"
-      click_on "Update"
-
-      expect(page).to have_content("White Cat Twosie")
-      expect(page).to have_content("39.99")
-    end
-
     it "store manager can't access other stores items edit/new" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_2)
 
