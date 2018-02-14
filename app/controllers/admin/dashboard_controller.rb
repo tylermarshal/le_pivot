a4a11391f149f8acbaa9827b8dee068fe64aaac9
TMad <madsen.tyler@gmail.com>
2018-02-14 14:46:30 -0700

    remove adminstrator flash notice

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,5 @@ class Admin::DashboardController < ApplicationController
 
   def index
     @orders = Order.check_for_status(params)
-    flash[:notice] = "You're logged in as an Administrator."
   end
 end
