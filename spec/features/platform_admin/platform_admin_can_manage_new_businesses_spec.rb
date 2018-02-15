82dd830d5f34091f6d0608d6df602c06b93529c3
katyjane8 <katy.jane8@gmail.com>
2018-02-15 12:20:25 -0700

    all tests passing

--- spec/features/platform_admin/platform_admin_can_manage_new_businesses_spec.rb
+++ spec/features/platform_admin/platform_admin_can_manage_new_businesses_spec.rb
@@ -11,7 +11,9 @@ describe "Platform admin can approve new business creation" do
 
     expect(current_path).to eq(admin_dashboard_index_path)
     expect(page).to have_content "Administrator"
-    click_on "Manage Businesses"
+    within("#myTab") do
+      click_on "Manage Businesses"
+    end
     expect(current_path).to eq(admin_stores_path)
   end
 
@@ -28,16 +30,8 @@ describe "Platform admin can approve new business creation" do
     expect(page).to have_content "Administrator"
     click_on "Manage Businesses"
     expect(current_path).to eq(admin_stores_path)
-    #within store button?
-    #expect kitten store to be pending
     expect(page).to have_content("pending")
-    # and it has approve and deny buttons
     expect(page).to have_link("Approve")
-    #expect puppy store to be active
-    # expect(page).to have_content("active")
-    # and it has no buttons
-    #expect bunny store to be denied
-    # and it has no buttons
   end
 
   it "created store defaults to pending state" do
@@ -51,11 +45,8 @@ describe "Platform admin can approve new business creation" do
     expect(page).to have_content "Administrator"
     click_on "Manage Businesses"
     expect(page).to have_content("Kitten Store")
-    # expect it to be in pending status
     expect(page).to have_content("pending")
-    # click on Approve button
     click_on "Approve"
-    # expect kitten store to now have approved status
     expect(page).to have_content("active")
   end
 end
