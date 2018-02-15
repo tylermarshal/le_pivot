3b8b0942738bea4a4ceddcf3adbb3ee6f5716aa0
katyjane8 <katy.jane8@gmail.com>
2018-02-14 17:07:07 -0700

    fix merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :new, :create, :edit, :update]
 
   resources :orders, only: [:index, :new, :show, :update]
+  resources :charges, only: [:create]
 
   resources :dashboard, only: [:index]
 
