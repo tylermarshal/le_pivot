e378def23fa9f1e0174a881bb444585fbc73b714
katyjane8 <katy.jane8@gmail.com>
2018-02-14 21:44:16 -0700

    admin stores controller holds all stores regardless of status

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,5 +1,8 @@
 class Admin::StoresController < ApplicationController
   def index
-
+    @stores = []
+    @stores << Store.where(status: 'pending')
+    @stores << Store.where(status: 'active')
+    @stores << Store.where(status: 'denied')
   end
 end
