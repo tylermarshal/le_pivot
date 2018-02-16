df445dbb400db16e77f64c37831c7028cb9c2fa6
TMad <madsen.tyler@gmail.com>
2018-02-15 20:23:57 -0700

    add api key email testing

--- spec/features/user/authentication/user_can_register_as_developer_spec.rb
+++ spec/features/user/authentication/user_can_register_as_developer_spec.rb
@@ -14,14 +14,31 @@ describe "a logged in user visits their dashboard" do
     within(".footer") do
       click_on "Developer Settings"
     end
-    expect(current_path).to eq ("/settings/developer")
+    expect(current_path).to eq("/settings/developer")
 
     click_on "Register"
 
-    expect(current_path).to eq ("/settings/developer")
+    expect(current_path).to eq("/settings/developer")
+
+    expect(page).to have_content("Your API key has been sent to you via email.")
 
     expect(page).to have_css(".api_key")
 
     expect(user.developer.api_key).to_not be_empty
+
+  end
+  it "user receives an email with api key" do
+    user = User.create(first_name: "Tester", last_name: "McTesty", email: "testerson@testmail.com", password: "testing")
+    role = Role.create(title: "registered_user")
+
+    user.roles << role
+    create(:developer, user: user, api_key: "asdfasdf")
+
+    mail = DeveloperMailer.api_key_email(user)
+
+    expect(mail.subject).to eq("Your HomeBodies API Key")
+    expect(mail.to).to eq([user.email])
+    expect(mail.from).to eq(["no-reply@homebodies.com"])
+    expect(mail.body.encoded).to match("Thanks")
   end
 end
