b1b37f9bfd6c03834b0b0c43e37212fd746dd23a
TMad <madsen.tyler@gmail.com>
2018-02-15 20:29:41 -0700

    add link back to dashboard from developer settings

--- spec/mailers/previews/developer_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/developer_mailer
-class DeveloperMailerPreview < ActionMailer::Preview
-  def sample_mail_preview
-     DeveloperMailer.api_key_email(User.all[1])
-   end
-end
