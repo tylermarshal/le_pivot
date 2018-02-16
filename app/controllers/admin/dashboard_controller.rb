62a36579d80c6ccb12bc322eec6d2ce331b9a108
TMad <madsen.tyler@gmail.com>
2018-02-16 09:46:31 -0700

    remove order limitations

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,5 @@
 class Admin::DashboardController < ApplicationController
   def index
-    if current_user.platform_admin?
-      @orders = Order.check_for_status(params)
-    else
-      @orders = current_user.stores.orders
-    end
+    @orders = Order.check_for_status(params)
   end
 end
