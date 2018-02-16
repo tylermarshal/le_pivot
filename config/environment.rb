34b0b75adc4750653b174bc9b8307808221ddc43
TMad <madsen.tyler@gmail.com>
2018-02-15 20:30:44 -0700

    add mailer environment config settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["email_username"],
+  :password => ENV["email_password"],
+  :domain => 'le-pivot-nouveau.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
